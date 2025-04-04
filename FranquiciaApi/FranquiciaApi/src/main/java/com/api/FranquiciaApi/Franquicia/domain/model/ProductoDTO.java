d312b1f55810293e3a634da83f6d1297265f95be
Daniela Forero <dpfa1507@gmail.com>
2025-04-03 22:24:17 -0500

    primer version

---
+++ FranquiciaApi/FranquiciaApi/src/main/java/com/api/FranquiciaApi/Franquicia/domain/model/ProductoDTO.java
@@ -0,0 +1,4 @@
+package com.api.FranquiciaApi.Franquicia.domain.model;
+
+public class ProductoDTO {
+}
---
+++ FranquiciaApi/FranquiciaApi/src/main/java/com/api/FranquiciaApi/Franquicia/infraestructure/drivenadapter/mysql/model/ProductoEntity.java
@@ -0,0 +1,4 @@
+package com.api.FranquiciaApi.Franquicia.infraestructure.drivenadapter.mysql.model;
+
+public class ProductoEntity {
+}
